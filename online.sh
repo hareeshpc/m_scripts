c39d691afc5799ee6a8d99754120e414974ca784
Hareesh Puthalath <hputhala@cisco.com>
2017-10-27 19:00:47 -0700

    Update check online script

--- online.sh
+++ online.sh
@@ -5,6 +5,7 @@ if ! hash wget 2>/dev/null; then
 	yum install -y wget
 else
 	echo "wget is present.Continuing..."
+fi
 
 wget -q --tries=10 --timeout=20 --spider http://google.com
 if [[ $? -eq 0 ]]; then
