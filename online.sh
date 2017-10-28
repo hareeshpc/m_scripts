a852cbc707b6daa704633130d7a7764f716ef9c9
Hareesh Puthalath <hputhala@cisco.com>
2017-10-27 18:58:51 -0700

    Add check online script

---
+++ online.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if ! hash wget 2>/dev/null; then
+        echo "Wget not present. Trying to install"
+	yum install -y wget
+else
+	echo "wget is present.Continuing..."
+
+wget -q --tries=10 --timeout=20 --spider http://google.com
+if [[ $? -eq 0 ]]; then
+        echo "Online"
+else
+        echo "Offline"
+fi
